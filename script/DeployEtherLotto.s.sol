7c5363e5e18efaf14d512b12975d4d900815f704
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-10-02 22:32:20 +0100

    Create DeployEtherLotto.s.sol

---
+++ script/DeployEtherLotto.s.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {EtherLotto} from "../src/EtherLotto.sol";
+import {HelperConfig} from "./config/HelperConfig.s.sol";
+import {CreateSubscription} from "./Interactions.s.sol";
+
+contract DeployEtherLotto is Script {
+    function run() public {}
+
+    function deployContracts() public returns (EtherLotto, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+
+        if (config.subscriptionId == 0) {
+            CreateSubscription createSubscription = new CreateSubscription();
+            (
+                config.subscriptionId,
+                config.vrfCoordinatorV2_5
+            ) = createSubscription.createSubscription(
+                config.vrfCoordinatorV2_5
+                // config.account
+            );
+        }
+
+        vm.startBroadcast();
+        EtherLotto etherLotto = new EtherLotto(
+            config.lotteryEntranceFee,
+            config.automationUpdateInterval,
+            config.vrfCoordinatorV2_5,
+            config.gasLane,
+            config.subscriptionId,
+            config.callbackGasLimit
+        );
+        vm.stopBroadcast();
+
+        return (etherLotto, helperConfig);
+    }
+}
